7116982edfaefba600d4d30114aeaade6e5aeda0
owozsh <vbs.owozsh@gmail.com>
2021-02-23 00:30:59 -0300

    add nvm and vim plug

--- .zshrc
+++ .zshrc
@@ -6,6 +6,9 @@ plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 alias org="nvim ~/茶/LIBRARY/org/.org.md"
 alias cnvim="nvim ~/.config/nvim/init.vim"
 alias ga="git add ."
--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 rm -rf ~/.zshrc
 rm -rf ~/.config/nvim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
