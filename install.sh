1a89bf60ae8113cf209d2f6a4701f9d53e736cf5
owozsh <vbs.owozsh@gmail.com>
2021-03-25 14:41:49 -0300

    add folders on installation

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir ~/.config/kitty
+mkdir ~/.config/nvim
+
 ln -sf ~/Developer/gitclones/dotfiles/zshrc ~/.zshrc
 ln -sf ~/Developer/gitclones/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/Developer/gitclones/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
