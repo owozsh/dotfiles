4a3d9e0d6e20e5b7881fe2edb8a2e78106552f7a
owozsh <owozsh@posteo.net>
2021-08-10 09:05:46 -0300

    update author

--- install.sh
+++ install.sh
@@ -9,13 +9,9 @@ git clone https://github.com/ergenekonyigit/lambda-gitster.git
 cd lambda-gitster
 cp lambda-gitster.zsh-theme ~/.oh-my-zsh/custom/themes
 cd ..
-rm lambda-gitster
+rm -rf lambda-gitster
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
