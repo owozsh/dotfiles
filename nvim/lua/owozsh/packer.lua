f8586c812dc257566d2407be1c3160810ee12198
Vítor Barroso <contact@owozsh.dev>
2023-09-02 17:00:17 -0300

    setup lsp zero

--- nvim/lua/owozsh/packer.lua
+++ nvim/lua/owozsh/packer.lua
@@ -11,6 +11,21 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
 
 	-- themes
 	use "ellisonleao/gruvbox.nvim"
