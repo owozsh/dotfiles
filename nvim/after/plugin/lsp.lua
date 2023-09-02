4a02e121103335f790f371fbeb7c8446ed422e14
Vítor Barroso <contact@owozsh.dev>
2023-09-02 17:08:32 -0300

    update lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -9,4 +9,27 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+require("mason-lspconfig").setup {
+    ensure_installed = {
+	    "elixirls",
+	    "volar",
+	    "tailwindcss",
+	    "pyright",
+	    "solargraph",
+	    "marksman",
+	    "ltex",
+	    "tsserver",
+	    "eslint",
+	    "dockerls",
+	    "docker_compose_language_service",
+	    "emmet_ls",
+	    "bashls",
+	    "clangd",
+	    "cssls",
+	    "jsonls",
+	    "lua_ls",
+	    "rust_analyzer"
+    },
+}
+
 lsp.setup()
