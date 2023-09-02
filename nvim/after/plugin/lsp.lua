01cec321c80fa3634e19a35962ebc05ab0ff18ed
Vítor Barroso <contact@owozsh.dev>
2023-09-02 17:31:17 -0300

    setup autocomplete

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,35 +1,47 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 require("mason-lspconfig").setup {
-    ensure_installed = {
-	    "elixirls",
-	    "volar",
-	    "tailwindcss",
-	    "pyright",
-	    "solargraph",
-	    "marksman",
-	    "ltex",
-	    "tsserver",
-	    "eslint",
-	    "dockerls",
-	    "docker_compose_language_service",
-	    "emmet_ls",
-	    "bashls",
-	    "clangd",
-	    "cssls",
-	    "jsonls",
-	    "lua_ls",
-	    "rust_analyzer"
-    },
+	ensure_installed = {
+		"elixirls",
+		"volar",
+		"tailwindcss",
+		"pyright",
+		"solargraph",
+		"marksman",
+		"ltex",
+		"tsserver",
+		"eslint",
+		"dockerls",
+		"docker_compose_language_service",
+		"emmet_ls",
+		"bashls",
+		"clangd",
+		"cssls",
+		"jsonls",
+		"lua_ls",
+		"rust_analyzer"
+	},
 }
 
 lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+	mapping = {
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+	},
+	preselect = 'item',
+	completion = {
+		completeopt = 'menu,menuone,noinsert'
+	},
+})
