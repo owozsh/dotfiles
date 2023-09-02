434eb546f8e166abfde63d71cc394bc27e62a061
Vítor Barroso <contact@owozsh.dev>
2023-09-02 17:44:31 -0300

    update nvim sets

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -35,10 +35,13 @@ require("mason-lspconfig").setup {
 lsp.setup()
 
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
 	mapping = {
 		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
 	},
 	preselect = 'item',
 	completion = {
