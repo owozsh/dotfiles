8cdb351a7797431d7cb2c82360bf7febf9212bd0
Vítor Barroso <v77@posteo.com>
2026-05-12 14:07:56 -0300

    Update neovim config

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,14 +1,17 @@
-require("rose-pine").setup({
-    styles = {
-      bold = true,
-      italic = true,
-      transparency = true,
-  },
-})
-
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd.colorscheme("flexoki")
+require("gruvbox").setup({
+  bold = false,
+  italic = {
+    comments = false,
+    emphasis = false,
+    folds = false,
+    operators = false,
+    strings = false
+  },
+  transparent_mode = true
+})
+vim.cmd.colorscheme("gruvbox")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
